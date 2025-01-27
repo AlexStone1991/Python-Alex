74b908b8a2d2bc6f9b98be470579e1b823fc9222
Alex <solo-drive@bk.ru>
2025-01-27 15:51:34 +0300

    hw_21 - Написал функцию get_image_paths

--- moya domashka_Python/domashla_21.py
+++ moya domashka_Python/domashla_21.py
@@ -40,3 +40,27 @@ def compress_image(file_path: str, quality: int = 50, format: str = "avif") -> N
     if format == "heic":
             heif_file = heif_from_pillow(image)
             heif_file.save(new_file_path, quality=quality)
+
+def get_image_paths(source_path: str, allowed_extensions: list[str]) -> list[str]:
+    """
+    Возвращает список путей к изображениям с поддерживаемыми расширениями в указанной директории.
+
+    :param source_path: Путь к директории или файлу.
+    :param allowed_extensions: Список поддерживаемых расширений.
+    :return: Список путей к изображениям.
+    """
+    if not os.path.exists(source_path):
+        raise ValueError(f"Путь {source_path} не существует")
+
+    if os.path.isfile(source_path):
+        return [source_path]
+
+    images = []
+    for root, dirs, files in os.walk(source_path):
+        for file in files:
+            full_path = os.path.join(root, file)
+            if os.path.isfile(full_path):
+                if file.split('.')[-1] in allowed_extensions:
+                    images.append(full_path)
+
+    return images
