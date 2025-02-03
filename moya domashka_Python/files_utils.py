bcfe9f3cc465c4c48b1831dea92195d2c98a242e
Alex <solo-drive@bk.ru>
2025-02-03 21:18:52 +0300

    hw_22 - добавление функций для работы с CSV.

--- moya domashka_Python/files_utils.py
+++ moya domashka_Python/files_utils.py
@@ -25,3 +25,31 @@ def append_json(*data: dict, file_path: str, encoding: str = "utf-8") -> None:
     existing_data.extend(data)
     write_json(existing_data, file_path, encoding)
 
+# Функции для работы с CSV
+# 4 Функция read_csv
+
+import csv
+
+def read_csv(file_path: str, delimiter=';', encoding: str = 'utf-8-sig') -> list:
+    """Читает данные из CSV-файла."""
+    with open(file_path, 'r', encoding=encoding) as file:
+        reader = csv.DictReader(file, delimiter=delimiter)
+        data = [row for row in reader]
+    return data
+
+# 5 Функция write_csv
+def write_csv(*data: dict, file_path: str, delimiter=';', encoding: str = 'utf-8-sig') -> None:
+    """Записывает данные в CSV-файл."""
+    with open(file_path, 'w', encoding=encoding, newline='') as file:
+        writer = csv.DictWriter(file, fieldnames=data[0].keys(), delimiter=delimiter)
+        writer.writeheader()
+        writer.writerows(data)
+
+# 6 Функция append_csv
+def append_csv(*data: dict, file_path: str, delimiter=';', encoding: str = 'utf-8-sig') -> None:
+    """Добавляет данные в существующий CSV-файл."""
+    with open(file_path, 'a', encoding=encoding, newline='') as file:
+        writer = csv.DictWriter(file, fieldnames=data[0].keys(), delimiter=delimiter)
+        writer.writerows(data)
+
+
