d259cb7f20f0dcc2882f0bcf71debb0cdbc028fb
Alex <solo-drive@bk.ru>
2025-02-03 21:42:25 +0300

    hw_22 - Исправление ошибок

--- moya domashka_Python/files_utils.py
+++ moya domashka_Python/files_utils.py
@@ -1,7 +1,6 @@
 import json
 import csv
 import yaml
-from typing import List, Dict, Any
 
 # Функции для работы с JSON
 
@@ -12,7 +11,7 @@ def read_json(file_path: str, encoding: str = "utf-8") -> dict:
         data = json.load(file)
     return data
 
-# Функция write_json
+# 2 Функция write_json
 def write_json(*data: dict, file_path: str, encoding: str = "utf-8") -> None:
     """Записывает данные в JSON-файл."""
     with open(file_path, 'w', encoding=encoding) as file:
@@ -22,14 +21,14 @@ def write_json(*data: dict, file_path: str, encoding: str = "utf-8") -> None:
 def append_json(*data: dict, file_path: str, encoding: str = "utf-8") -> None:
     """Добавляет данные в существующий JSON-файл."""
     existing_data = read_json(file_path, encoding)
-    existing_data.extend(data)
-    write_json(existing_data, file_path, encoding)
+    if isinstance(existing_data, list):
+        existing_data.extend(data)
+    else:
+        existing_data = [existing_data] + list(data)
+    write_json(*existing_data, file_path=file_path, encoding=encoding)
 
 # Функции для работы с CSV
 # 4 Функция read_csv
-
-import csv
-
 def read_csv(file_path: str, delimiter=';', encoding: str = 'utf-8-sig') -> list:
     """Читает данные из CSV-файла."""
     with open(file_path, 'r', encoding=encoding) as file:
