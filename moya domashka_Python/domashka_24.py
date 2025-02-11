42f5e12325a5886f78f92b85de16dc5f3863c6a1
Alex <solo-drive@bk.ru>
2025-02-11 21:28:05 +0300

    hw_24 - фильтрация на букву Ч, преобразование года в строку,

--- moya domashka_Python/domashka_24.py
+++ moya domashka_Python/domashka_24.py
@@ -26,3 +26,24 @@ filtered_list = list(filter(lambda x: x["id"] in processed_input, repacked_list)
 
 print("Отфильтрованный список:", filtered_list)
 
+# Шаг 5: Множество уникальных режиссеров
+
+# Множество уникальных режиссеров
+unique_directors = {movie["director"] for movie in full_dict.values()}
+
+print("Уникальные режиссеры:", unique_directors)
+
+# Шаг 6: Преобразование года в строку
+# Создаем копию full_dict, где значение ключа year преобразовано в строку.
+# Преобразование года в строку
+year_as_string = {key: {**value, "year": str(value["year"])} for key, value in full_dict.items()}
+
+print("Год как строка:", year_as_string[list(year_as_string.keys())[0]])  # Выводим первый элемент для примера
+
+# Шаг 7: Фильтрация фильмов, начинающихся на букву "Ч"
+# Используем filter, чтобы оставить только те фильмы, название которых начинается на букву "Ч".
+
+# Фильтрация фильмов, начинающихся на "Ч"
+filtered_by_title = list(filter(lambda x: x["title"].startswith("Ч"), full_dict.values()))
+
+print("Фильмы, начинающиеся на 'Ч':", filtered_by_title)
