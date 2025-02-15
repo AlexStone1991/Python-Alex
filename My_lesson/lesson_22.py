c9f0ed8cc5bfcb20f4f6ecd11deafc63f659b72b
Alex <solo-drive@bk.ru>
2025-02-15 15:09:05 +0300

    lesson_22 - мини обьявление для авито с стоймостью на платное продвижение!

--- My_lesson/lesson_22.py
+++ My_lesson/lesson_22.py
@@ -4,21 +4,35 @@
 # - нейминг классов UpperCamelCase
 # - __init__ - иницилизатор
 #  - self - ссылка на экземпляр класса
+# - __new__ - как скрытая часть конструктора
+# - методы работающие с self - методы экземпляра
+# - документация класса и методов
 
-class Person:
-    def __init__(self, name: str):
-        self.name = name
-    def say_my_name(self):
-        print(f"Привет, меня зовут {self.name}!")
+# класс обЪявления
+class AdPost:
+    promote_rate: float = 0.1
+    def __init__(self, title: str, text: str, price: int):
+        self.title = title
+        self.text = text
+        self.price = price
+        # self.promote_rate: float = 0.5
+    
+    def __str__(self) -> str:
+        return f"Заголовок: {self.title}, Текст: {self.text[:20]}, цена: {self.price}"
+    
+    def calculate_promote_cost(self, day: int) -> int:
+        promote_cost = int(self.price * (self.promote_rate / 100) * day)
+        return promote_cost
 
-p1 = Person("Барак")
-p2 = Person("Владимир") 
-p3 = Person("Джордж")
-
-print(p1.name, p2.name, p3.name)
-p1.say_my_name()
-p2.say_my_name()
-p3.say_my_name()
 
 
+ap1 = AdPost("Sony playstation 5. Муха не сидела!", "Новая, красивая, блестящая...!", 20000)
+ap2 = AdPost("Видеокарта RTX 3090", "Как новая", 60000)
+
+AdPost.promote_rate = 1
+
+print(ap1)
+print(ap2)
+print(ap1.calculate_promote_cost(3),"рублей")
+print(ap2.calculate_promote_cost(3),"рублей")
 
