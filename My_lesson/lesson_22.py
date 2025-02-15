4a198c587ed109925679312f7135f3bed4348f78
Alex <solo-drive@bk.ru>
2025-02-15 14:23:27 +0300

    lesson_22 - методы и атрибуты экземпляра

--- My_lesson/lesson_22.py
+++ My_lesson/lesson_22.py
@@ -1,19 +1,24 @@
 # TODO ООП. Ч1. Атрибуты и методы. Класс и экземпляр. Практика. Урок 22.
 
-#  -class
+#  - class
 # - нейминг классов UpperCamelCase
+# - __init__ - иницилизатор
+#  - self - ссылка на экземпляр класса
 
 class Person:
-    # Атрибут класса
-    name = "Alexander"
+    def __init__(self, name: str):
+        self.name = name
+    def say_my_name(self):
+        print(f"Привет, меня зовут {self.name}!")
 
-p1 = Person()
-p2 = Person() 
+p1 = Person("Барак")
+p2 = Person("Владимир") 
+p3 = Person("Джордж")
 
-print(p1.name, p2.name)
+print(p1.name, p2.name, p3.name)
+p1.say_my_name()
+p2.say_my_name()
+p3.say_my_name()
 
-p1.name = "Филлип"
-p2.name = "Джордж"
 
-print(p1.name, p2.name)
 
