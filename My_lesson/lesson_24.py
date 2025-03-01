cf5ca65a897c8da9172d968f8a0c982f3f2b7e03
Alex <solo-drive@bk.ru>
2025-03-01 15:22:17 +0300

    lesson_24 - класс TabulateTable для работы с табличными данными с инкапсуляцией

--- My_lesson/lesson_24.py
+++ My_lesson/lesson_24.py
@@ -3,73 +3,88 @@
 # _ это protected - доступно при наследовании
 # __ это private - доаступно только внутри класса
 
-class Car:
-    def __init__(self, model: str, color: str):
-        self.model = model
-        self.color = color
-        self.__speed = 0 # protected
-        self.__max_speed = 200 # private
+# собественные getters и  setters
+# @property
+# @speed.setter
+# @speed.deleter
 
-    def __str__(self):
-        return f"Марка: {self.model}, Максимальная скорость: {self.__max_speed}"
+# https://pypi.org/project/tabulate/
 
-    def __validate_speed(self, speed: int):
-        """
-        Приватный метод-валидатор скорости.
-        Проверяет, что скорость не отрицательная и не превышает максимальную.
-        """
-        if speed < 0:
-            raise ValueError("Скорость не может быть отрицательной")
-        
-        if speed > self.__max_speed:
-            raise ValueError("Скорость не может превышать максимальную скорость")
-        
+available_formats = [
+            "plain", "simple", "github", "grid", "simple_grid", 
+            "rounded_grid", "heavy_grid", "mixed_grid", "double_grid", 
+            "fancy_grid", "outline", "simple_outline", "rounded_outline", 
+            "heavy_outline", "mixed_outline", "double_outline", 
+            "fancy_outline", "pipe", "orgtbl", "asciidoc", "jira", 
+            "presto", "pretty", "psql", "rst", "mediawiki", 
+            "moinmoin", "youtrack", "html", "unsafehtml", 
+            "latex", "latex_raw", "latex_booktabs", "latex_longtable", 
+            "textile", "tsv"
+        ]
+
+from tabulate import tabulate
+from typing import List, Dict, Union, Any, Optional
+
+class TabulateTable:
+    
+    __awaitable_styles = [
+            "plain", "simple", "github", "grid", "simple_grid", 
+            "rounded_grid", "heavy_grid", "mixed_grid", "double_grid", 
+            "fancy_grid", "outline", "simple_outline", "rounded_outline", 
+            "heavy_outline", "mixed_outline", "double_outline", 
+            "fancy_outline", "pipe", "orgtbl", "asciidoc", "jira", 
+            "presto", "pretty", "psql", "rst", "mediawiki", 
+            "moinmoin", "youtrack", "html", "unsafehtml", 
+            "latex", "latex_raw", "latex_booktabs", "latex_longtable", 
+            "textile", "tsv"
+        ]
+    def __init__(self):
+        self.__data: Union[List[Dict[str, Any]], List[List[Any]]] = []
+        self.__style: str = "pretty"
+        self.__headers: Optional[List[str]] = None
+        self.__tupe_data: Optional[str] = None
+    
     @property
-    def speed(self) -> int:
-        # Публичный метод для получения скорости
-        return self.__speed
+    def style(self):
+        return self.__style
+    
+    @style.setter
+    def style(self, style: str):
+        if style in self.__awaitable_styles:
+            self.__style = style
+        else:
+            raise ValueError(f"такого стиля нет. Выберите из {self.__awaitable_styles}")
+    @property
+    def data(self) -> List[Dict[str, Any]] | List[List[Any]]:
+        return self.__data
+    @data.setter
+    def data(self, data: Union[List[Dict[str, Any]], List[List[Any]]]):
+        self.__type_data = self.__validate_data(data)
+        self.__data = data
         
-    @speed.setter
-    def speed(self, speed: int):
-        """
-        Публичный метод для установки скорости.
-        Вызывает приватный метод-валидатор.
-        """
-        self.__validate_speed(speed)
-        self.__speed = speed
-    @speed.deleter
-    def speed(self):
-        self.__speed = 0
+    def __validate_data(self, data: Union[List[Dict[str, Any]], List[List[Any]]]):
+        if isinstance(data[0], dict):
+            return "dicts"
+        elif isinstance(data[0], list):
+            return "lists"
+        else:
+            raise ValueError("данные должны быть списками словарей или списками значений")
+        
+    def render(self) -> str:
+        if self.__type_data == "dicts":
+            return tabulate(self.__data, headers="keys", tablefmt=self.__style)
+        elif self.__type_data == "lists":
+            return tabulate(self.__data, tablefmt=self.__style)
+        else:
+            raise ValueError("неверные данные для рендеринга таблицы")
+        
+if __name__ == "__main__":
+    table = TabulateTable()
+    table.data = [
+        {"name": "John", "age": 25},
+        {"name": "Alice", "age": 30},
+    ]
+    table.style = "github"
+    print(table.render())
 
-# class Driver:
-#     def __init__(self, name: str, car: Car):
-#         self.name = name
-#         self.car = car
-
-#     def drive(self, speed: int):
-#         print(f"{self.name} сел в {self.car.model}")
-#         self.car.set_speed(speed)
-#         print(f"{self.name} едет со скоростью {self.car._speed}")
-
-audi = Car("Audi", "red")
-print(audi)
-audi.speed = 100
-print(audi.speed)
-audi.speed = 200
-print(audi.speed)
-del audi.speed
-print(audi.speed)
-
-# volga = Car("Volga", "Black")
-# driver = Driver("Иосиф", volga)
-# driver.drive(100)
-# driver.drive(200)
-# driver.drive(300)
-# print(volga.model)
-# print(volga.color)
-# print(volga._speed)
-# # print(volga.__max_speed)
-# print(volga._Car__max_speed)
-
-#  шаблон искажения имени _<ClassName>__<attributeName>
-
+    
