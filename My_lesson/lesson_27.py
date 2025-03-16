fef827754cf5ac698739f5ff6929331f4f03882c
Alex <solo-drive@bk.ru>
2025-03-16 13:54:37 +0300

    lesson_27 - рабочий вариант миксинов

--- My_lesson/lesson_27.py
+++ My_lesson/lesson_27.py
@@ -1,33 +1,37 @@
 # TODO ООП. Ч6. Наследование. Миксины. Практика. Урок 27.
 
-# Пример использования миксинов
 
 class Pizza:
-    def __init__(self, size: int):
-        self.size = size
+    def __init__(self, **kwargs):
+        self.size = kwargs.get('size', 30)
+        super().__init__(**kwargs)
 
 class Pie:
-    def __init__(self, size: int):
-        self.size = size
+    def __init__(self, **kwargs):
+        self.size = kwargs.get('size', 30)
+        super().__init__(**kwargs)
 
 class CheeseBorderMixin:
-    def __init__(self, height: int):
-        self.height = height
+    def __init__(self, **kwargs):
+        self.height = kwargs.get('height', 5)
     def add_cheese_border(self):
         print(f"Сырный борт, высотой {self.height} мм Активирован!")
 
 class ThinkCrustMixin:
-    def __init__(self, thickness: int):
-        self.thickness = thickness
+    def __init__(self, **kwargs):
+        self.thickness = kwargs.get('thickness', 1)
     def add_thin_crust(self):
         print(f"Тонкое тексто, толщиной {self.thickness} мм Активировано!")
+# Пицца с сырным бортом
+class CheeseBorderPizza(Pizza, CheeseBorderMixin):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.add_cheese_border()
+# тест
+print("Пицца с сырным бортом")
+pizza = CheeseBorderPizza(size=40, height=20)
+print(pizza.size)
+print(pizza.height)
+print(pizza.__dict__)
 
-# 1 нам нужна Пицца Делаем экземплят класса Pizza
-# pizza = Pizza(30)
 
-# 2 нам нужна мутация - пицца с сырным бортом делаем экземпляр класса PizzaCheeseBorder
-
-class PizzaWithCheeseBorder(Pizza, CheeseBorderMixin):
-    def __init__(self, size: int, height: int):
-        Pizza.__init__(self, size)
-        CheeseBorderMixin.__init__(self, height)
