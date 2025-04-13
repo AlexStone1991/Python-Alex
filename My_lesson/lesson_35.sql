68792316499a883300ac9cc182f359cb79041383
Alex <solo-drive@bk.ru>
2025-04-13 16:34:07 +0300

    lesson_35 - Тема SQL. Ч1. Установка софта. Первые SELECT. Урок 35.

--- My_lesson/lesson_35.sql
+++ My_lesson/lesson_35.sql
@@ -3,97 +3,109 @@
 SELECT *
 FROM MarvelCharacters;
 
--- SQL - декларативная язык, мы описываем то что хотим получить, а не как это сделать.
+-- SQL - декларативный язык, мы описываем то, что хотим получить, а не как это сделать
 
--- SEELECT - команда для получения данных из базы данных
+-- SELECT - выбираем данные из таблицы
 -- FROM - из какой таблицы
--- WHERE - фильтруем данные по определенному условию
--- ORDER BY - сортируем данные по определенному полю
--- LIMIT - ограничиваем количество выводимых данных
--- OFFSET - пропускаем определенное количество строк
+-- WHERE - фильтруем данные по определённому условию
+-- ORDER BY - сортируем данные по определённому полю
+-- LIMIT - ограничиваем количество возвращаемых строк
+-- OFFSET - пропускаем определённое количество строк
 -- DISTINCT - выбираем только уникальные значения
 
-SELECT name, EYE, HAIR, SEX, APPEARANCES
+SELECT name, EYE, HAIR, SEX, Appearances
 FROM MarvelCharacters
 WHERE APPEARANCES > 1000
 ORDER BY APPEARANCES DESC;
 
--- WHERE - доступны булевы вырожения как в Python
--- > < >= <= = !=
--- AND OR NOT
+-- В WHERE доступны булевы выражения, как в Python
+-- >, <, >=, <=, !=, =
+-- AND, OR, NOT
 -- BETWEEN - между двумя значениями
--- IN в списке значений
--- LIKE - поиск по шаблону
--- IS NULL - проверка на NULL\
-
--- Только '' одинарные кавычки
--- название столбцов, псевдонимы, названия таблиц можно в двойных кавычках
--- регистрозависимость для поиска, но не для называния столбцов и таблиц
-
+-- IN - в списке значений
+-- LIKE - по шаблону
+-- IS NULL - проверка на NULL
 
 SELECT name, APPEARANCES
 FROM MarvelCharacters
 WHERE HAIR = 'Bald' AND APPEARANCES > 10
 ORDER BY APPEARANCES DESC;
 
+-- Строки для поиска обязательно в одиночных кавычках ''
+-- Названия столбцов, псевдонимы, названия таблиц можно в двойных кавычках
+-- Регистрозависимость для поиска, но не для названия столбцов и таблиц
+
+-- DISTINCT - выбираем только уникальные значения
 SELECT DISTINCT HAIR
 FROM MarvelCharacters;
--- DISTINCT - выбирает только уникальные значения
+
 SELECT DISTINCT EYE
 FROM MarvelCharacters;
 
 SELECT DISTINCT EYE, HAIR
 FROM MarvelCharacters
 WHERE EYE IS NOT NULL AND HAIR IS NOT NULL;
+-- NULL - это не значение, а отсутствие значения
 
+
+--PRACTICE Добудьте уникальные значения по полу SEX
+-- Выберите интересное вам значение, и найдите всех персонажей этим полом
 SELECT DISTINCT SEX
 FROM MarvelCharacters;
 
-SELECT name, SEX
+SELECT name, APPEARANCES, year, SEX
 FROM MarvelCharacters
-WHERE SEX = 'Agender Characters';
+WHERE SEX = 'Genderfluid Characters' OR SEX = 'Agender Characters'
+ORDER BY APPEARANCES DESC;
 
 SELECT *
 FROM MarvelCharacters
 WHERE year IS NOT NULL
-ORDER BY year;
+ORDER BY year DESC;
 
-SELECT *
-FROM MarvelCharacters
+-- Персонажи с 39 по 45
+SELECT * FROM MarvelCharacters 
 WHERE 
     year IS NOT NULL
     AND year > 1939
     AND year < 1946;
--- Вариант 2. BEETWEEN мы можем указать диапазон значений
-SELECT * FROM MarvelCharacters
-WHERE
+
+-- Вариант 2. BEETWEEN - мы можем указать диапазон значений
+SELECT * FROM MarvelCharacters 
+WHERE 
     year IS NOT NULL
     AND year BETWEEN 1939 AND 1945;
 
--- Третий вариант IN вхождение в список значений
-SELECT * fROM MarvelCharacters
-WHERE
+-- IN - вхождение в список значений
+SELECT * FROM MarvelCharacters 
+WHERE 
     year IS NOT NULL
     AND year IN (1939, 1940, 1941, 1942, 1943, 1944, 1945);
 
 -- Ищем "злодеев в базе" (hitler, stalin, lenin)
+
 -- Ищем по цвету волос ('Black Hair', 'White Hair', 'Red Hair')
--- попробуйте сделать запрос похожий, но HAIR in этот кортеж
-SELECT * FROM MarvelCharacters
+
+-- Попробуйте сделать запрос похожий, но hair in этот кортеж
+SELECT * FROM MarvelCharacters 
 WHERE 
-    HAIR IN ('Black Hair', 'White Hair', 'Red Hair')
+    hair  IN ('Black Hair', 'White Hair', 'Red Hair')
     AND APPEARANCES > 10
 ORDER BY hair, APPEARANCES DESC;
 
-SELECT * FROM MarvelCharacters
+-- LIMIT
+-- OFFSET
+SELECT * FROM MarvelCharacters 
 ORDER BY APPEARANCES DESC
 LIMIT 10
 OFFSET 10;
 
--- LIKE - похоже на или КАК
--- % - любое колличество символов
+
+
+-- LIKE- похоже на или КАК
+-- % - любое количество символов
 -- _ - один символ
 
 SELECT name, APPEARANCES, year
-FROM MarvelCharacters 
-WHERE name LIKE '%Spider%man%';
+FROM MarvelCharacters
+WHERE name LIKE '%Spider%'
